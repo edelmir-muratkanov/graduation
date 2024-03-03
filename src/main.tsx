1a956f23353e094b487b4f96120b83e8495ec090
Edelmir <muratkanov02@gmail.com>
2024-03-03 16:37:36 +0500

    chore: add deps
    * add axios
    * add shadcn ui
    * add tanstack query
    * add tanstack router

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+import '@/assets/index.css'
+
 const root = document.getElementById('root')!
 
 createRoot(root).render(
