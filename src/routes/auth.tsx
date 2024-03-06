af9aee1db99580f9a13db2c5590cf40e5b81423e
Edelmir <muratkanov02@gmail.com>
2024-03-06 06:49:44 +0500

    fix(auth): :bug: set auth default stage, set default redirect url in auth page

--- src/routes/auth.tsx
+++ src/routes/auth.tsx
@@ -4,11 +4,8 @@ import { z } from 'zod'
 import { AuthLoading } from '@/pages/auth/loading'
 
 const authSearchSchema = z.object({
-  stage: z
-    .enum(['login', 'register'] as const)
-    .optional()
-    .catch('login'),
-  redirectUrl: z.string().optional(),
+  stage: z.enum(['login', 'register'] as const).catch('login'),
+  redirectUrl: z.string().catch('/'),
 })
 
 export const Route = createFileRoute('/auth')({
