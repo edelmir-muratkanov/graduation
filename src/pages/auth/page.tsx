e9f1c1f79c7171a2bf053e8fdbdffdbdfc33abf0
Edelmir <muratkanov02@gmail.com>
2024-03-07 22:44:21 +0500

    refactor(projects): :recycle: remove importing route, instead get from function

--- src/pages/auth/page.tsx
+++ src/pages/auth/page.tsx
@@ -1,10 +1,11 @@
-import { Route } from '@/routes/auth'
+import { getRouteApi } from '@tanstack/react-router'
 
 import { LoginForm } from './LoginForm/login-form'
 import { RegisterForm } from './RegisterForm/register-form'
 
 export const AuthPage = () => {
-  const { stage } = Route.useSearch()
+  const { useSearch } = getRouteApi('/auth')
+  const { stage } = useSearch()
 
   return stage === 'login' ? <LoginForm /> : <RegisterForm />
 }
