c7964aa71463a1a2c9ffcde0a88b853ced87c851
Edelmir <muratkanov02@gmail.com>
2024-03-10 18:53:35 +0500

    feat(projects): :sparkles: add masking to project route

--- src/pages/project/useProjectPage.ts
+++ src/pages/project/useProjectPage.ts
@@ -1,17 +1,29 @@
 import { getRouteApi, useNavigate } from '@tanstack/react-router'
 
+export type Tab = 'info' | 'calculations'
+
 export const useProjectPage = () => {
-  const { useSearch } = getRouteApi('/projects/$projectId/')
+  const { useSearch, useParams } = getRouteApi('/projects/$projectId/')
   const navigate = useNavigate()
 
   const { tab } = useSearch()
+  const params = useParams()
 
   const hanleTabsValueChange = (value: string) =>
     navigate({
+      to: '/projects/$projectId',
+      params,
       search: {
-        tab: value,
+        tab: value as Tab,
+      },
+      mask: {
+        to: '/projects/$projectId',
+        params,
       },
     })
 
-  return { state: { tab }, functions: { hanleTabsValueChange } }
+  return {
+    state: { tab: tab as Tab },
+    functions: { hanleTabsValueChange },
+  }
 }
