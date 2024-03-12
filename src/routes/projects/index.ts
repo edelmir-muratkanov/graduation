7dfc0147e5d230d7d68621a28ec9db6c98bc29d1
Edelmir <muratkanov02@gmail.com>
2024-03-12 19:16:02 +0500

    fix(projects): :bug: add default params to prevent  unnesesaryrequest

--- src/routes/projects/index.ts
+++ src/routes/projects/index.ts
@@ -5,7 +5,9 @@ import { ProjectsLoading } from '@/pages/projects/loading'
 
 export const Route = createFileRoute('/projects/')({
   loader: ({ context }) => {
-    context.queryClient.ensureQueryData(getProjectsQueryOptions())
+    context.queryClient.ensureQueryData(
+      getProjectsQueryOptions({ config: { params: { limit: 10, offset: 0 } } }),
+    )
   },
   pendingComponent: ProjectsLoading,
 })
