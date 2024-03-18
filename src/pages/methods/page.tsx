718df74bd1d5c3283dd3da46465c2995bb53a04a
Edelmir <muratkanov02@gmail.com>
2024-03-18 22:33:16 +0500

    fix(methods): :lock: render create method link only for admins

--- src/pages/methods/page.tsx
+++ src/pages/methods/page.tsx
@@ -1,17 +1,21 @@
 import { Link } from '@tanstack/react-router'
 
 import { Button, Heading } from '@/components/ui'
+import { useProfile } from '@/lib/contexts'
 
 import { MethodsTable } from './methods-table/methods-table'
 
 export const MethodsPage = () => {
+  const { user } = useProfile()
   return (
     <div className='w-full h-full space-y-4'>
       <div className='flex justify-between items-center'>
         <Heading as='h1'>Methods</Heading>
-        <Link to='/methods/new'>
-          <Button variant='secondary'>Create new method</Button>
-        </Link>
+        {user?.role === 'Admin' ? (
+          <Link to='/methods/new'>
+            <Button variant='secondary'>Create new method</Button>
+          </Link>
+        ) : null}
       </div>
 
       <MethodsTable />
