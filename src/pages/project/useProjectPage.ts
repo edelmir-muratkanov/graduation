a3564673254e1df574a9c680eced54f526e8049d
Edelmir <muratkanov02@gmail.com>
2024-03-19 22:32:59 +0500

    feat(projects): :sparkles: return project name

--- src/pages/project/useProjectPage.ts
+++ src/pages/project/useProjectPage.ts
@@ -1,25 +1,32 @@
 import { getRouteApi, useNavigate } from '@tanstack/react-router'
 
+import { useGetProjectQuery } from '@/lib/api'
+
 export type Tab = 'info' | 'calculations'
 
 export const useProjectPage = () => {
   const { useSearch, useParams } = getRouteApi('/projects/$projectId/')
   const navigate = useNavigate()
 
-  const { tab } = useSearch()
+  const { tab = 'info' } = useSearch()
   const params = useParams()
+  const {
+    data: {
+      data: { name },
+    },
+  } = useGetProjectQuery(params.projectId)
 
-  const hanleTabsValueChange = (value: string) =>
+  const hanleTabsValueChange = (value: Tab) =>
     navigate({
       to: '/projects/$projectId',
       params,
       search: {
-        tab: value as Tab,
+        tab: value,
       },
     })
 
   return {
-    state: { tab: tab as Tab },
+    state: { tab, project: name },
     functions: { hanleTabsValueChange },
   }
 }
