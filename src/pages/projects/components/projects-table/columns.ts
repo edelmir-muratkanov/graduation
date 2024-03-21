3f23012d3cbf7c2449245d1f2928d4a194f8d621
Edelmir <muratkanov02@gmail.com>
2024-03-21 22:53:13 +0500

    refactor(projects): :recycle: tranlate to russian column headers & enums

--- src/pages/projects/components/projects-table/columns.ts
+++ src/pages/projects/components/projects-table/columns.ts
@@ -1,46 +1,50 @@
 import type { ColumnDef } from '@tanstack/react-table'
 
+import { CollectorTypeTranslates, ProjectTypeTranslates } from '@/lib/constants'
+
 export const COLUMNS: ColumnDef<Project & ProjectStatistic>[] = [
   {
     accessorKey: 'name',
-    header: 'Name',
+    header: 'Название',
     footer: props => props.column.id,
   },
   {
     accessorKey: 'country',
-    header: 'Country',
+    header: 'Страна',
     footer: props => props.column.id,
   },
   {
     accessorKey: 'operator',
-    header: 'Operator',
-    footer: props => props.column.id,
-  },
-  {
-    accessorKey: 'collectorType',
-    header: 'Collector Type',
+    header: 'Оператор',
     footer: props => props.column.id,
   },
   {
     accessorKey: 'type',
-    header: 'Project type',
+    header: 'Тип проекта',
+    cell: info => ProjectTypeTranslates[info.getValue() as ProjectType],
     footer: props => props.column.id,
   },
   {
-    header: 'Methods',
+    accessorKey: 'collectorType',
+    header: 'Тип коллектора',
+    cell: info => CollectorTypeTranslates[info.getValue() as CollectorType],
+    footer: props => props.column.id,
+  },
+  {
+    header: '№ методов',
     accessorFn: row => row._count.methods,
     cell: info => info.getValue(),
     footer: props => props.column.id,
   },
 
   {
-    header: 'Properties',
+    header: '№ параметров',
     accessorFn: row => row._count.parameters,
     cell: info => info.getValue(),
     footer: props => props.column.id,
   },
   {
-    header: 'Users',
+    header: '№ пользователей',
     accessorFn: row => row._count.users,
     cell: info => info.getValue(),
     footer: props => props.column.id,
