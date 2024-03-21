3ebe30b855647f0a9266ddbc3dfc2fd00b3c15db
Edelmir <muratkanov02@gmail.com>
2024-03-21 21:30:52 +0500

    feat: add logo

--- src/components/header/nav.tsx
+++ src/components/header/nav.tsx
@@ -1,11 +1,20 @@
 import { Link } from '@tanstack/react-router'
 
+import { LogoIcon } from '../icons'
+
 export const Nav = () => {
   return (
     <div className='mr-4 flex w-full'>
       <Link to='/' className='mr-6 flex items-center space-x-2'>
-        <span>LOGO</span>
-        <span className='font-bold inline-block'>Graduation</span>
+        <LogoIcon className='size-10' />
+        <div>
+          <span className='font-oswald font-semibold text-xl tracking-widest uppercase p-0'>
+            Eor
+          </span>
+          <p className='text-sm font-oswald tracking-wide leading-none'>
+            Selection
+          </p>
+        </div>
       </Link>
 
       <nav className='flex items-center'>
@@ -26,7 +35,7 @@ export const Nav = () => {
               to='/methods'
               className='hover:text-foreground/80'
               activeProps={{
-                className: 'text-foreground',
+                className: 'text-foreground/90 font-semibold',
               }}
             >
               Methods
