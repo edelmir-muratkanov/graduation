467c65b0c238a7b855ce1490b2867dd16cbd9e33
Edelmir <muratkanov02@gmail.com>
2024-03-27 07:19:07 +0500

    refactor(projects): :recycle: remove areas

--- src/pages/project/project-calculations/project-calculations.tsx
+++ src/pages/project/project-calculations/project-calculations.tsx
@@ -2,7 +2,6 @@ import {
   CartesianGrid,
   Line,
   LineChart,
-  ReferenceArea,
   ResponsiveContainer,
   Tooltip,
   XAxis,
@@ -45,40 +44,27 @@ export const ProjectCalculations = () => {
   const { state } = useProjectCalculations()
 
   return (
-    <TabsContent value='calculations'>
-      <ResponsiveContainer height={400}>
-        <LineChart data={state.calculations}>
-          <XAxis dataKey='method.name' tickMargin={10} />
-          <YAxis
-            domain={[-1.25, 1.25]}
-            ticks={[-1, -0.75, -0.5, -0.25, 0, 0.25, 0.5, 0.75, 1]}
-          />
-          <Tooltip content={<CustomTooltip />} />
-          <CartesianGrid strokeDasharray='5 5' />
+    <TabsContent value='calculations' className='space-y-5'>
+      <div className='w-full h-96'>
+        <ResponsiveContainer>
+          <LineChart data={state.calculations}>
+            <XAxis
+              dataKey='method.name'
+              tick={{ fontSize: 12 }}
+              tickMargin={10}
+              padding={{ left: 30, right: 30 }}
+            />
+            <YAxis
+              domain={[-1.25, 1.25]}
+              ticks={[-1, -0.75, -0.5, -0.25, 0, 0.25, 0.5, 0.75, 1]}
+            />
+            <Tooltip content={<CustomTooltip />} />
+            <CartesianGrid strokeDasharray='5 5' />
 
-          <ReferenceArea label='Не применим' y2={-0.75} fill='#FF0D0D' />
-          <ReferenceArea label='Применим' y1={0.25} y2={0.75} fill='#ACB334' />
-          <ReferenceArea
-            label='Не благоприятен для применения'
-            y1={-0.75}
-            y2={-0.25}
-            fill='#FF4E11'
-          />
-          <ReferenceArea
-            label='Применим с низкой эффективностью'
-            y1={-0.25}
-            y2={0.25}
-            fill='#FF8E15'
-          />
-          <ReferenceArea
-            label='Благоприятен для применения'
-            y1={0.75}
-            fill='#69B34C'
-          />
-
-          <Line dataKey='ratio' activeDot={{ r: 10 }} />
-        </LineChart>
-      </ResponsiveContainer>
+            <Line type='monotone' dataKey='ratio' activeDot={{ r: 10 }} />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
     </TabsContent>
   )
 }
