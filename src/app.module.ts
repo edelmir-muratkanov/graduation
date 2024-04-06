062d1102eaf3e9dae360b0070259a58f713c3ed6
Edelmir <muratkanov02@gmail.com>
2024-04-06 21:43:29 +0500

    fix: :bug: fix redis cache ttl

--- src/app.module.ts
+++ src/app.module.ts
@@ -73,12 +73,11 @@ import { UsersModule } from './users/users.module'
 						host: process.env.REDIS_HOST,
 						port: parseInt(process.env.REDIS_PORT, 10),
 					},
-					// url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+					ttl: 30_000,
 				})
 
 				return {
 					store: store as unknown as CacheStore,
-					ttl: 30_000,
 				}
 			},
 		}),
