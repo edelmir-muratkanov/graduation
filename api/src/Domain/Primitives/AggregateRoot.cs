dc05f70b6654234b1ad0d7d501840fb35e5f9278
Edelmir <muratkanov02@gmail.com>
2024-04-17 04:57:16 +0500

    feat(api): :sparkles: add domain primitives
    add aggregate root, entity, domainEvent

---
+++ api/src/Domain/Primitives/AggregateRoot.cs
@@ -0,0 +1,18 @@
+﻿namespace Domain.Primitives;
+
+public abstract class AggregateRoot : Entity
+{
+    private readonly List<DomainEvent> _domainEvents = [];
+
+    protected AggregateRoot(Guid id) : base(id)
+    {
+    }
+
+    protected AggregateRoot()
+    {
+    }
+
+    public IReadOnlyCollection<DomainEvent> DomainEvents => _domainEvents.ToList();
+    public void ClearDomainEvents() => _domainEvents.Clear();
+    protected void RaiseDomainEvent(DomainEvent domainEvent) => _domainEvents.Add(domainEvent);
+}
