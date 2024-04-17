ed2812f6cf61d6c9498c8deb13758ffbd2a1f00d
Edelmir <muratkanov02@gmail.com>
2024-04-17 08:37:29 +0500

    feat(api): :sparkles: register persistence

--- api/src/WebApi/Program.cs
+++ api/src/WebApi/Program.cs
@@ -1,7 +1,11 @@
+using Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddPersistence(builder.Configuration);
 
 var app = builder.Build();
 
