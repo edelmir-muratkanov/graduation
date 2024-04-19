867cada5e88412aae09f72d7a0592b77382e508f
Edelmir <muratkanov02@gmail.com>
2024-04-20 01:00:17 +0500

    feat(api/property): :sparkles: add get properties feature

--- api/Api/Features/Property/GetProperties.cs
+++ api/Api/Features/Property/GetProperties.cs
@@ -1,6 +1,58 @@
-﻿namespace Api.Features.Property;
+﻿using Api.Infrastructure.Database;
+using Api.Shared.Messaging;
+using Api.Shared.Models;
+using Carter;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
 
-public class GetProperties
+namespace Api.Features.Property;
+
+public class GetPropertiesEndpoint : ICarterModule
 {
-    
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app
+            .MapGroup("api/properties")
+            .MapGet("", async (string? searchTerm, ISender sender, CancellationToken cancellationToken) =>
+            {
+                var query = new GetProperties.Query(searchTerm);
+                var result = await sender.Send(query, cancellationToken);
+
+                return result.Match(Results.Ok, CustomResults.Problem);
+            })
+            .Produces<PaginatedList<GetProperties.Response>>()
+            .Produces(500)
+            .WithName("Get properties")
+            .WithTags("properties");
+    }
+}
+
+public static class GetProperties
+{
+    public record Response(Guid Id, string Name, string Unit);
+
+    public record Query(string? SearchTerm, int PageNumber = 1, int PageSize = 10) : IQuery<PaginatedList<Response>>;
+
+    internal sealed class Handler(ApplicationDbContext context) : IQueryHandler<Query, PaginatedList<Response>>
+    {
+        public async Task<Result<PaginatedList<Response>>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            var propertiesQuery = context.Properties.AsNoTracking();
+
+            if (!string.IsNullOrWhiteSpace(request.SearchTerm))
+            {
+                propertiesQuery = propertiesQuery.Where(p => p.Name.Contains(request.SearchTerm));
+            }
+
+            var properties = propertiesQuery.Select(p => new Response(p.Id, p.Name, p.Unit));
+
+            var list = await PaginatedList<Response>.CreateAsync(
+                properties,
+                request.PageNumber,
+                request.PageSize,
+                cancellationToken);
+
+            return list;
+        }
+    }
 }
