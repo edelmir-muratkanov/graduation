e0b560d0bfaefc3d7350d427760d8d26f6fdbb5b
Edelmir <muratkanov02@gmail.com>
2024-04-22 10:13:39 +0500

    feat(api/method): :sparkles: append add method parameters feature

---
+++ api/Api/Contracts/Method/AddMethodParameters.cs
@@ -0,0 +1,15 @@
+﻿namespace Api.Contracts.Method;
+
+public record ValueGroup
+{
+    public required double Min { get; init; }
+    public required double Avg { get; init; }
+    public required double Max { get; init; }
+}
+
+public record AddMethodParametersRequest
+{
+    public required Guid PropertyId { get; init; }
+    public ValueGroup? First { get; init; }
+    public ValueGroup? Second { get; init; }
+}
