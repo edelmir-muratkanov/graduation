60081ca174e1626891413f7b01d38b0355623e6d
Edelmir <muratkanov02@gmail.com>
2024-04-25 03:40:56 +0500

    test(api/calculations): :white_check_mark: add unit tests
    * add calculation entity tests
    * add calculation item entity tests
    * add belonging value object tests

---
+++ api/Domain.UnitTests/Calculation/BelongingTests.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Calculation;
+using FluentAssertions;
+
+namespace Domain.UnitTests.Calculation;
+
+public class BelongingTests
+{
+    [Theory]
+    [InlineData(-2)]
+    [InlineData(-1.1)]
+    [InlineData(2)]
+    [InlineData(1.01)]
+    public void Belonging_Should_ThrowArgumentOutOfRangeException_WhenDegreeIsInvalid(double degree)
+    {
+        Belonging Action() => new(degree);
+
+        FluentActions.Invoking(Action).Should().Throw<ArgumentOutOfRangeException>()
+            .Which
+            .ParamName.Should().Be("degree");
+    }
+
+    [Theory]
+    [InlineData(-0.8, Belonging.NotApplicable)]
+    [InlineData(-0.5, Belonging.NotSuitable)]
+    [InlineData(-0.1, Belonging.LowEfficiency)]
+    [InlineData(0.5, Belonging.Applicable)]
+    [InlineData(0.8, Belonging.Favorable)]
+    public void Belonging_Should_HaveProperStatus_WhenDegreeIsValid(double degree, string status)
+    {
+        var belonging = new Belonging(degree);
+
+        belonging.Status.Should().Be(status);
+    }
+}
---
+++ api/Domain.UnitTests/Calculation/CalculationItemTests.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Calculation;
+using FluentAssertions;
+
+namespace Domain.UnitTests.Calculation;
+
+public class CalculationItemTests
+{
+    [Fact]
+    public void Create_Should_CreateItem_When_BelongingIsValid()
+    {
+        Belonging belonging = new(0.7);
+
+        var calculationItem = CalculationItem.Create(Guid.NewGuid(), "Some name", belonging);
+
+        calculationItem.Should().NotBeNull();
+    }
+}
