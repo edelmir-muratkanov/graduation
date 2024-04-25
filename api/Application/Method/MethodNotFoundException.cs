1b21e7e60af2b7cb2a5a5a1ce58b3f4968c65c75
Edelmir <muratkanov02@gmail.com>
2024-04-26 02:52:36 +0500

    feat(api): add not found exceptions

---
+++ api/Application/Method/MethodNotFoundException.cs
@@ -0,0 +1,6 @@
+﻿namespace Application.Method;
+
+public sealed class MethodNotFoundException(Guid methodId) : Exception($"Method with id = {methodId} not found")
+{
+    public Guid MethodId { get; set; } = methodId;
+}
---
+++ api/Application/Project/ProjectNotFoundException.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.Project;
+
+public sealed class ProjectNotFoundException(Guid projectId)
+    : Exception($"The project with id = {projectId} not found")
+{
+    public Guid ProjectId { get; } = projectId;
+}
