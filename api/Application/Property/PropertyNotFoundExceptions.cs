962bae3ac68c31f02b6db5880bb377ab0afe154a
Edelmir <muratkanov02@gmail.com>
2024-04-28 11:01:47 +0500

    feat(api/property): add not found exception

---
+++ api/Application/Property/PropertyNotFoundExceptions.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.Property;
+
+public sealed class PropertyNotFoundException(Guid propertyId)
+    : Exception($"Property with id = {propertyId} not found")
+{
+    public Guid PropertyId { get; set; } = propertyId;
+}
