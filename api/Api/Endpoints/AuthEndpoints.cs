c69778015c35bda9bd400c3cd06d2a2444ad7563
Edelmir <muratkanov02@gmail.com>
2024-05-01 18:54:51 +0500

    refactor(api:auth): :recycle: moved api handlers to class methods

--- api/Api/Endpoints/AuthEndpoints.cs
+++ api/Api/Endpoints/AuthEndpoints.cs
@@ -8,9 +8,7 @@ using Infrastructure.Authentication;
 using MediatR;
 using Microsoft.Extensions.Options;
 using Shared.Results;
-using LoginResponse = Api.Contracts.Auth.LoginResponse;
-using RefreshResponse = Api.Contracts.Auth.RefreshResponse;
-using RegisterResponse = Api.Contracts.Auth.RegisterResponse;
+using AuthContracts = Api.Contracts.Auth;
 
 namespace Api.Endpoints;
 
@@ -18,121 +16,133 @@ public class AuthEndpoints : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        RouteGroupBuilder? group = app.MapGroup("api/auth").WithTags("auth");
+        RouteGroupBuilder? group = app
+            .MapGroup("api/auth")
+            .WithTags("auth")
+            .WithOpenApi();
 
-        group.MapGet("profile", async (
-                ISender sender, CancellationToken cancellationToken) =>
-            {
-                var query = new GetProfileQuery();
-                Result<GetProfileResponse>? result = await sender.Send(query, cancellationToken);
 
-                return result.Match(Results.Ok, CustomResults.Problem);
-            })
+        group.MapGet("profile", MapGetProfile)
             .RequireAuthorization()
             .Produces<GetProfileResponse>()
             .Produces(401)
+            .WithSummary("Get user profile")
             .WithName("Get profile");
 
-        group.MapPost("login", async (
-                LoginCommand request,
-                ISender sender,
-                HttpContext context,
-                IOptions<JwtOptions> jwtOptions,
-                CancellationToken cancellationToken) =>
-            {
-                Result<Application.Auth.Login.LoginResponse>? result = await sender.Send(request, cancellationToken);
-
-                if (result.IsFailure)
-                {
-                    return CustomResults.Problem(result);
-                }
-                context.Response.Cookies.Append(AuthConstants.RefreshTokenKey, result.Value.RefreshToken,
-                    new CookieOptions
-                    {
-                        Expires = DateTimeOffset.Now.AddDays(jwtOptions.Value.RefreshExpiryInDays)
-                    });
-
-                var response = new LoginResponse(
-                    result.Value.Id,
-                    result.Value.Email,
-                    result.Value.Role,
-                    result.Value.AccessToken);
-
-                return Results.Ok(response);
-            })
-            .Produces<LoginResponse>()
+        group.MapPost("login", MapPostLogin)
+            .Produces<AuthContracts.LoginResponse>()
             .ProducesProblem(400)
             .ProducesProblem(404)
             .ProducesProblem(500)
+            .WithSummary("Login user")
             .WithName("Login");
 
-        group.MapPost("refresh", async (
-                ISender sender,
-                HttpContext context,
-                IOptions<JwtOptions> jwtOptions,
-                CancellationToken cancellationToken) =>
-            {
-                string? access = context.Request.Headers.Authorization[0]?.Split(" ")[1];
-                string? refresh = context.Request.Cookies[AuthConstants.RefreshTokenKey];
-
-                var command = new RefreshCommand(access!, refresh!);
-
-                Result<Application.Auth.Refresh.RefreshResponse>? result =
-                    await sender.Send(command, cancellationToken);
-
-                if (result.IsFailure)
-                {
-                    return Results.Unauthorized();
-                }
-
-                var response = new RefreshResponse(result.Value.AccessToken);
-                
-                context.Response.Cookies.Append(AuthConstants.RefreshTokenKey, result.Value.RefreshToken,
-                    new CookieOptions
-                    {
-                        Expires = DateTimeOffset.Now.AddDays(jwtOptions.Value.RefreshExpiryInDays)
-                    });
-
-                return Results.Ok(response);
-            })
-            .Produces<RefreshResponse>()
+        group.MapPost("refresh", MapPostRefresh)
+            .Produces<AuthContracts.RefreshResponse>()
             .Produces(401)
             .ProducesProblem(500)
+            .WithSummary("Refresh tokens")
             .WithName("Refresh");
 
-        group.MapPost("register", async (
-                RegisterCommand request,
-                HttpContext context,
-                ISender sender,
-                IOptions<JwtOptions> jwtOptions,
-                CancellationToken cancellationToken) =>
-            {
-                Result<Application.Auth.Register.RegisterResponse>? result =
-                    await sender.Send(request, cancellationToken);
-
-                if (result.IsFailure)
-                {
-                    return CustomResults.Problem(result);
-                }
-                
-                context.Response.Cookies.Append(AuthConstants.RefreshTokenKey, result.Value.RefreshToken,
-                    new CookieOptions
-                    {
-                        Expires = DateTimeOffset.Now.AddDays(jwtOptions.Value.RefreshExpiryInDays)
-                    });
-
-                var response = new RegisterResponse(
-                    result.Value.Id,
-                    result.Value.Email,
-                    result.Value.Role,
-                    result.Value.AccessToken);
-
-                return Results.Created("api/auth/profile", response);
-            })
-            .Produces<RegisterResponse>(201)
+        group.MapPost("register", MapPostRegister)
+            .Produces<AuthContracts.RegisterResponse>(201)
             .ProducesProblem(400)
             .ProducesProblem(409)
             .ProducesProblem(500)
+            .WithSummary("Register user")
             .WithName("Register");
+    }
+
+    private static async Task<IResult> MapPostRegister(
+        RegisterCommand request,
+        HttpContext context,
+        ISender sender,
+        IOptions<JwtOptions> jwtOptions,
+        CancellationToken cancellationToken)
+    {
+        Result<RegisterResponse> result = await sender.Send(request, cancellationToken);
+
+        if (result.IsFailure)
+        {
+            return CustomResults.Problem(result);
+        }
+
+        context.Response.Cookies.Append(AuthConstants.RefreshTokenKey, result.Value.RefreshToken, new CookieOptions
+        {
+            Expires = DateTimeOffset.Now.AddDays(jwtOptions.Value.RefreshExpiryInDays)
+        });
+
+        var response = new AuthContracts.RegisterResponse(
+            result.Value.Id,
+            result.Value.Email,
+            result.Value.Role,
+            result.Value.AccessToken);
+
+        return Results.Created("api/auth/profile", response);
+    }
+
+    private static async Task<IResult> MapPostRefresh(
+        ISender sender,
+        HttpContext context,
+        IOptions<JwtOptions> jwtOptions,
+        CancellationToken cancellationToken)
+    {
+        string? access = context.Request.Headers.Authorization[0]?.Split(" ")[1];
+        string? refresh = context.Request.Cookies[AuthConstants.RefreshTokenKey];
+
+        var command = new RefreshCommand(access!, refresh!);
+
+        Result<RefreshResponse> result = await sender.Send(command, cancellationToken);
+
+        if (result.IsFailure)
+        {
+            return Results.Unauthorized();
+        }
+
+        var response = new AuthContracts.RefreshResponse(result.Value.AccessToken);
+
+        context.Response.Cookies.Append(AuthConstants.RefreshTokenKey, result.Value.RefreshToken, new CookieOptions
+        {
+            Expires = DateTimeOffset.Now.AddDays(jwtOptions.Value.RefreshExpiryInDays)
+        });
+
+        return Results.Ok(response);
+    }
+
+    private static async Task<IResult> MapPostLogin(
+        LoginCommand request,
+        ISender sender,
+        HttpContext context,
+        IOptions<JwtOptions> jwtOptions,
+        CancellationToken cancellationToken)
+    {
+        Result<LoginResponse> result = await sender.Send(request, cancellationToken);
+
+        if (result.IsFailure)
+        {
+            return CustomResults.Problem(result);
+        }
+
+        context.Response.Cookies.Append(AuthConstants.RefreshTokenKey, result.Value.RefreshToken, new CookieOptions
+        {
+            Expires = DateTimeOffset.Now.AddDays(jwtOptions.Value.RefreshExpiryInDays)
+        });
+
+        var response = new AuthContracts.LoginResponse(
+            result.Value.Id,
+            result.Value.Email,
+            result.Value.Role,
+            result.Value.AccessToken);
+
+        return Results.Ok(response);
+    }
+
+
+    private static async Task<IResult> MapGetProfile(ISender sender, CancellationToken cancellationToken)
+    {
+        var query = new GetProfileQuery();
+        Result<GetProfileResponse>? result = await sender.Send(query, cancellationToken);
+
+        return result.Match(Results.Ok, CustomResults.Problem);
     }
 }
