7da00956bb9fa346be70c0a7ce4ab67d9b3d6ed2
Edelmir <muratkanov02@gmail.com>
2024-05-04 19:39:29 +0500

    build(web): :rocket: add docker file

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -7,18 +7,25 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
   const API_URL = env.VITE_SERVER_URL ?? ''
   const APP_PORT = parseInt(env.VITE_APP_PORT, 10)
+  const PREVIEW_PORT = parseInt(env.VITE_PREVIEW_PORT, 10)
 
   return {
     plugins: [react(), TanStackRouterVite()],
 
     server: {
       port: APP_PORT,
+      strictPort: true,
+      host: true,
       proxy: {
         '/api': {
           target: API_URL,
           rewrite: path => path.replace(/^\/api/, ''),
         },
       },
+    },
+    preview: {
+      port: PREVIEW_PORT,
+      strictPort: true,
     },
 
     resolve: {
