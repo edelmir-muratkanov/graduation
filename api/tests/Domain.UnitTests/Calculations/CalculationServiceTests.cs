ed9cbf4769a7bd1b4c90526ef77a05135f02fc6c
Edelmir <muratkanov02@gmail.com>
2024-05-04 17:53:32 +0500

    test(api:calculations): add more tests to calculate belonging

--- api/tests/Domain.UnitTests/Calculations/CalculationServiceTests.cs
+++ api/tests/Domain.UnitTests/Calculations/CalculationServiceTests.cs
@@ -37,28 +37,57 @@ public class CalculationServiceTests
         FluentActions.Invoking(Action).Should().Throw<InvalidOperationException>();
     }
 
-    [Fact]
-    public void CalculateBelongingDegree_Should_ReturnProperBelonging_When_MethodFirstParameterAbsent()
+    [Theory]
+    [InlineData(2.31, 35, 36, 37, 1)]
+    [InlineData(2.31, 0.3, 0.5, 1, -1)]
+    [InlineData(2.31, 0.2, 0.6, 1, -1)]
+    [InlineData(783, 834, 840, 850, 1)]
+    public void CalculateBelongingDegree_Should_ReturnProperBelonging_When_MethodFirstParameterAbsent(
+        double x,
+        double min,
+        double avg,
+        double max,
+        double expected)
     {
-        var second = new ParameterValueGroup(35, 36, 37);
-        Belonging result = _calculationService.CalculateBelongingDegree(2.31, null, second);
-        result.Degree.Should().Be(-1);
+        var second = new ParameterValueGroup(min, avg, max);
+        Belonging result = _calculationService.CalculateBelongingDegree(x, null, second);
+        Math.Round(result.Degree, 4).Should().Be(expected);
     }
 
-    [Fact]
-    public void CalculateBelongingDegree_Should_ReturnProperBelonging_When_MethodSecondParameterAbsent()
+    [Theory]
+    [InlineData(0.67, 0.4, 0.42, 0.43, 1)]
+    [InlineData(0.67, 0.75, 0.78, 0.8, -1)]
+    [InlineData(0.67, 0.15, 0.17, 0.2, 1)]
+    [InlineData(85, 80, 85, 87, 0.5)]
+    public void CalculateBelongingDegree_Should_ReturnProperBelonging_When_MethodSecondParameterAbsent(
+        double x,
+        double min,
+        double avg,
+        double max,
+        double expected)
     {
-        var first = new ParameterValueGroup(0.03, 0.07, 0.1);
-        Belonging result = _calculationService.CalculateBelongingDegree(0.19, first, null);
-        result.Degree.Should().Be(1);
+        var first = new ParameterValueGroup(min, avg, max);
+        Belonging result = _calculationService.CalculateBelongingDegree(x, first, null);
+        Math.Round(result.Degree, 4).Should().Be(expected);
     }
 
-    [Fact]
-    public void CalculateBelongingDegree_Should_ReturnProperBelonging_When_MethodParametersPresent()
+    [Theory]
+    [InlineData(1861, 457, 800, 1000, 3800, 4000, 4075, 1)]
+    [InlineData(783, 801, 810, 820, 900, 910, 922, -1)]
+    [InlineData(2.31, 0.04, 5, 10, 286, 400, 18000, 0.0813)]
+    public void CalculateBelongingDegree_Should_ReturnProperBelonging_When_MethodParametersPresent(
+        double x,
+        double min1,
+        double avg1,
+        double max1,
+        double min2,
+        double avg2,
+        double max2,
+        double expected)
     {
-        var first = new ParameterValueGroup(0.04, 5, 10);
-        var second = new ParameterValueGroup(286, 400, 18000);
-        Belonging result = _calculationService.CalculateBelongingDegree(2.31, first, second);
-        Math.Round(result.Degree, 4).Should().Be(0.0813);
+        var first = new ParameterValueGroup(min1, avg1, max1);
+        var second = new ParameterValueGroup(min2, avg2, max2);
+        Belonging result = _calculationService.CalculateBelongingDegree(x, first, second);
+        Math.Round(result.Degree, 4).Should().Be(expected);
     }
 }
