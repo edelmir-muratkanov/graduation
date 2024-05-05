4191309dd2080c702969a202059f5573f513056d
Edelmir <muratkanov02@gmail.com>
2024-05-05 22:45:10 +0500

    refactor(api:auth): get profile
    get profile from database

---
+++ api/src/Infrastructure/Queries/Auth/GetProfileQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using Application.Abstractions.Authentication;
+using Application.Auth.GetProfile;
+using Domain.Users;
+
+namespace Infrastructure.Queries.Auth;
+
+internal sealed class GetProfileQueryHandler(
+    ICurrentUserService currentUserService,
+    ApplicationReadDbContext dbContext)
+    : IQueryHandler<GetProfileQuery, GetProfileResponse>
+{
+    public async Task<Result<GetProfileResponse>> Handle(GetProfileQuery request, CancellationToken cancellationToken)
+    {
+        UserReadModel? user = await dbContext.Users
+            .FirstOrDefaultAsync(u => u.Id.ToString() == currentUserService.Id, cancellationToken);
+
+        return user is not null
+            ? new GetProfileResponse(user.Id.ToString(), user.Email, user.Role.ToString())
+            : Result.Failure<GetProfileResponse>(UserErrors.Unauthorized);
+    }
+}
